0d38c5626b16870cd947acf88408a0909c15634a
Tirtha2107 <tirthakadam63@gmail.com>
2025-08-05 11:07:49 +0530

    first commoit mern

---
+++ backend/src/db/databaseConnection.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const DB_Name = "mern-blog";
+
+
+const connectDB = async() =>{
+    try{
+        const connect= await mongoose.connect(`${process.env.MONGODB_URL}/${DB_Name}`)
+        console.log("mongodb connected succesfully");
+        
+    }
+    catch(error)
+    {
+        console.log("mongodb error", error)
+    }
+}
+
+export default connectDB;
